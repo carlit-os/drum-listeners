0a7f2e31c4b060eb8b6972140a3c9161984d2468
Andy Moran <amoran2800@gmail.com>
2021-01-04 15:19:44 -0600

    removed .mp3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementByClassName("w drum").addEventListener("click", playAudio(tom-1.mp3))
+document.getElementByClassName("w drum").addEventListener("click", playAudio(tom-1))
 
 
 function playAudio(sound){
