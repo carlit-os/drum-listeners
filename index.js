b409461b77eb1ad309c919b96acdf9877ef10685
Andy Moran <amoran2800@gmail.com>
2021-01-04 16:48:51 -0600

    renamed mp3 with sounds/

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.getElementsByClassName("w drum").addEventListener("click", playAudio);
 
 
 function playAudio(sound){
-    var audio = new Audio(tom-1);
+    var audio = new Audio("sounds/tom-1.mp3");
     audio.play();
 }
